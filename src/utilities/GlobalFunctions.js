caa179905c123416f820522867e76f02514aa153
Abhinav Mahajan <mahajanabhinav.personal@gmail.com>
2022-11-05 20:04:58 +0530

    throttle Feature Added

--- src/utilities/GlobalFunctions.js
+++ src/utilities/GlobalFunctions.js
@@ -21,10 +21,32 @@ export const saveDataToLocalStorage = async toDoListData => {
 export const debounce = (func, delay) => {
   let debounceHandler;
   return function () {
-    const context = this;
-    const args = arguments;
+    const context = this,
+      args = arguments;
     // arguments is an object which is local to a function. it's a local variable that is available with all functions by default except arrow functions in JavaScript.
     clearTimeout(debounceHandler);
     debounceHandler = setTimeout(() => func.apply(context, args), delay);
+  };
+};
+
+// Throttling is a technique in which someFunction() is called only once in a specified time interval until the function-call-stream is over
+let inThrottle, lastFn, lastTime;
+export const throttle = (fn, wait) => {
+  return function () {
+    const context = this,
+      args = arguments;
+    if (!inThrottle) {
+      fn.apply(context, args);
+      lastTime = Date.now();
+      inThrottle = true;
+    } else {
+      clearTimeout(lastFn);
+      lastFn = setTimeout(function () {
+        if (Date.now() - lastTime >= wait) {
+          fn.apply(context, args);
+          lastTime = Date.now();
+        }
+      }, Math.max(wait - (Date.now() - lastTime), 0));
+    }
   };
 };
