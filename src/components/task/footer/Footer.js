477a46cfe44de48a1b898f414eb855c9252e3f60
Abhinav Mahajan <mahajanabhinav.personal@gmail.com>
2022-11-13 20:29:14 +0530

    Button Scaling Effect Added

--- src/components/task/footer/Footer.js
+++ src/components/task/footer/Footer.js
@@ -1,6 +1,11 @@
 import React, {useState} from 'react';
-import {Alert, KeyboardAvoidingView, TouchableHighlight} from 'react-native';
-import Animated from 'react-native-reanimated';
+import {
+  Animated,
+  Alert,
+  KeyboardAvoidingView,
+  TouchableHighlight,
+} from 'react-native';
+import Reanimated from 'react-native-reanimated';
 import {COLORS} from '../../../utilities/Colors';
 import {isiOS} from '../../../utilities/Constants';
 import {debounce} from '../../../utilities/Functions';
@@ -10,9 +15,28 @@ import {styles} from './Styles';
 
 const TaskListFooter = ({setter, animatedStyles}) => {
   const [toDoItem, setToDoItem] = useState();
+  const animatedButtonScale = new Animated.Value(1);
+
+  const onPressIn = () => {
+    Animated.spring(animatedButtonScale, {
+      toValue: 0.1,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const onPressOut = () => {
+    Animated.spring(animatedButtonScale, {
+      toValue: 1,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const animatedScaleStyle = {
+    transform: [{scale: animatedButtonScale}],
+  };
 
   return (
-    <Animated.View style={[styles.container, animatedStyles]}>
+    <Reanimated.View style={[styles.container, animatedStyles]}>
       <KeyboardAvoidingView
         behavior={isiOS ? 'padding' : 'height'}
         style={styles.taskInputWrapper}>
@@ -25,20 +49,24 @@ const TaskListFooter = ({setter, animatedStyles}) => {
         </AppTextInput>
       </KeyboardAvoidingView>
 
-      <TouchableHighlight
-        onPress={() => {
-          if (toDoItem) {
-            setter(toDoItem);
-            setToDoItem();
-          } else {
-            Alert.alert(Messages.taskTitle, Messages.invalidTaskInfo);
-          }
-        }}
-        style={styles.taskIconWrapper}
-        underlayColor={COLORS.basic500}>
-        <AppText style={styles.taskIcon}>+</AppText>
-      </TouchableHighlight>
-    </Animated.View>
+      <Animated.View style={[animatedScaleStyle]}>
+        <TouchableHighlight
+          onPressIn={onPressIn}
+          onPressOut={onPressOut}
+          onPress={() => {
+            if (toDoItem) {
+              setter(toDoItem);
+              setToDoItem();
+            } else {
+              Alert.alert(Messages.taskTitle, Messages.invalidTaskInfo);
+            }
+          }}
+          style={styles.taskIconWrapper}
+          underlayColor={COLORS.basic500}>
+          <AppText style={styles.taskIcon}>+</AppText>
+        </TouchableHighlight>
+      </Animated.View>
+    </Reanimated.View>
   );
 };
 
